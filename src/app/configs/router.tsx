47ccdd1b1a30cb7bd9ea748c802dffc590a7d869
Clément Llorens <bk201li@protonmail.com>
2023-01-11 21:13:35 +0100

    🚧 wip signup

--- src/app/configs/router.tsx
+++ src/app/configs/router.tsx
@@ -4,9 +4,8 @@ import App from '~/App';
 import YbookUtils from '~/@ybook/utils';
 import SignInUpConfig from '~/app/main/sign/sign-in-up/SignInUpConfig';
 import SignOutConfig from '~/app/main/sign/sign-out/SignOutConfig';
-import SignUpConfirmationConfig from '~/app/main/sign/sign-up-confirmation/SignUpConfirmationConfig';
 
-const routeConfigs = [SignInUpConfig, SignOutConfig, SignUpConfirmationConfig];
+const routeConfigs = [SignInUpConfig, SignOutConfig];
 
 export const router = createBrowserRouter([
   ...YbookUtils.generateRoutesFromConfigs(routeConfigs),
