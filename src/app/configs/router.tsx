9a3f88d9a4da99b33facb1762f1dce3c264256f6
Etienne Bachelard <newcorp.pro@gmail.com>
2023-01-11 17:56:18 +0100

    home page wip

--- src/app/configs/router.tsx
+++ src/app/configs/router.tsx
@@ -1,17 +1,18 @@
-import { createBrowserRouter } from 'react-router-dom';
-import App from '~/App';
+import { createBrowserRouter } from "react-router-dom";
+import App from "~/App";
 
-import YbookUtils from '~/@ybook/utils';
-import SignInConfig from '~/app/main/sign/sign-in/SignInConfig';
-import SignOutConfig from '~/app/main/sign/sign-out/SignOutConfig';
-import SignUp from '~/app/main/sign/sign-up/SignUpConfig';
+import YbookUtils from "~/@ybook/utils";
+import SignInConfig from "~/app/main/sign/sign-in/SignInConfig";
+import SignOutConfig from "~/app/main/sign/sign-out/SignOutConfig";
+import SignUp from "~/app/main/sign/sign-up/SignUpConfig";
+import Home from "~/app/main/home/HomeConfig";
 
-const routeConfigs = [SignInConfig, SignOutConfig, SignUp];
+const routeConfigs = [SignInConfig, SignOutConfig, SignUp, Home];
 
 export const router = createBrowserRouter([
   ...YbookUtils.generateRoutesFromConfigs(routeConfigs),
   {
-    path: '/',
+    path: "/",
     element: <App />,
   },
 ]);
