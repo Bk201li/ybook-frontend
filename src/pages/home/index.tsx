44343b2bec5567feab220595484cc42ea118a2cc
Etienne Bachelard <newcorp.pro@gmail.com>
2023-01-26 15:58:24 +0100

    bell done

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,40 +1,53 @@
-import React from 'react';
-import { useQuery } from '@tanstack/react-query';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import ModeCommentRoundedIcon from '@mui/icons-material/ModeCommentRounded';
-import CardHeader from '@mui/material/CardHeader';
-import IconButton from '@mui/material/IconButton';
-import Avatar from '@mui/material/Avatar';
-import { getPosts } from '~/store/api/posts';
-import { snackBar } from '~/store/atoms/snackBar';
-import { useAtom } from 'jotai';
-import { Box, ListItem, ListItemAvatar, ListItemText, TextField, Snackbar } from '@mui/material';
-import MuiAlert, { AlertProps } from '@mui/material/Alert';
-import { identity } from 'lodash';
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ModeCommentRoundedIcon from "@mui/icons-material/ModeCommentRounded";
+import CardHeader from "@mui/material/CardHeader";
+import IconButton from "@mui/material/IconButton";
+import Avatar from "@mui/material/Avatar";
+import { getPosts } from "~/store/api/posts";
+import { snackBar } from "~/store/atoms/snackBar";
+import { useAtom } from "jotai";
+import {
+  Box,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  TextField,
+  Snackbar,
+} from "@mui/material";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+import { identity } from "lodash";
 
-const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
 enum PostAction {
-  LIKE = 'like',
-  COMMENT = 'comment',
+  LIKE = "like",
+  COMMENT = "comment",
 }
 
 const Home: React.FC = () => {
-  const postsQuery = useQuery({ queryKey: ['posts'], queryFn: getPosts });
+  const postsQuery = useQuery({ queryKey: ["posts"], queryFn: getPosts });
   const [snackbar, setSnackbar] = useAtom(snackBar);
-  const [currentPostAction, setCurrentPostAction] = React.useState('');
+  const [currentPostAction, setCurrentPostAction] = React.useState("");
   const [isCommentOpen, setIsCommentOpen] = React.useState<null | number>(null);
   const [showMore, setShowMore] = React.useState(false);
 
-  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
-    if (reason === 'clickaway') {
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
       return;
     }
 
@@ -57,13 +70,13 @@ const Home: React.FC = () => {
           <div key={id}>
             <Card
               sx={{
-                position: 'relative',
-                top: '20px',
-                maxWidth: '100%',
-                maxHeight: '100%',
-                overflow: 'auto',
+                position: "relative",
+                top: "20px",
+                maxWidth: "100%",
+                maxHeight: "100%",
+                overflow: "auto",
                 borderRadius: 4,
-                boxShadow: '0px 2px 10px',
+                boxShadow: "0px 2px 10px",
                 px: 2,
                 my: 2,
               }}
@@ -71,11 +84,15 @@ const Home: React.FC = () => {
             >
               <CardHeader
                 avatar={<Avatar alt="profil" src={user.avatarS3Key} />}
-                titleTypographyProps={{ variant: 'h6', display: 'flex', justifyContent: 'start' }}
+                titleTypographyProps={{
+                  variant: "h6",
+                  display: "flex",
+                  justifyContent: "start",
+                }}
                 title={`${user.firstname} ${user.lastname}`}
-                sx={{ pb: '10px' }}
+                sx={{ pb: "10px" }}
               />
-              <CardContent sx={{ pt: '1px' }}>
+              <CardContent sx={{ pt: "1px" }}>
                 <Typography variant="body2" align="left">
                   {htmlContent}
                 </Typography>
@@ -87,27 +104,30 @@ const Home: React.FC = () => {
               sx={{ objectFit: 'contain', borderRadius: '15px' }}
             /> */}
               <CardActions className="flex justify-left">
-                <IconButton>
-                  <ThumbUpIcon sx={{ width: '30px', pr: '10px' }} />
+                <IconButton sx={{ outline: "0" }}>
+                  <ThumbUpIcon sx={{ width: "30px", ml: "10", border: "0" }} />
                 </IconButton>
-                <IconButton onClick={() => handleCommentOpen(id)}>
-                  <ModeCommentRoundedIcon sx={{ width: '30px', pl: '10px' }} />
+                <IconButton
+                  sx={{ outline: "0" }}
+                  onClick={() => handleCommentOpen(id)}
+                >
+                  <ModeCommentRoundedIcon sx={{ width: "30px", ml: "10" }} />
                 </IconButton>
               </CardActions>
               {isCommentOpen === id && (
                 <>
                   <Box
                     sx={{
-                      display: 'flex',
-                      alignItems: 'flex-end',
-                      borderTop: 'solid 1px gray',
-                      pl: '15px',
-                      py: '10px',
+                      display: "flex",
+                      alignItems: "flex-end",
+                      borderTop: "solid 1px gray",
+                      pl: "15px",
+                      py: "10px",
                     }}
                   >
                     {/* <Avatar alt="profil" src={avatar} /> */}
                     <TextField
-                      sx={{ pl: '15px' }}
+                      sx={{ pl: "15px" }}
                       placeholder="Écrivez votre réponse"
                       variant="standard"
                     />
@@ -123,15 +143,23 @@ const Home: React.FC = () => {
                   ))}
                   {postComments !== undefined && postComments.length > 3 && (
                     <IconButton onClick={() => setShowMore(!showMore)}>
-                      {showMore ? 'Afficher moins' : 'Afficher plus'}
+                      {showMore ? "Afficher moins" : "Afficher plus"}
                     </IconButton>
                   )}
                 </>
               )}
             </Card>
 
-            <Snackbar open={snackbar.isOpen} autoHideDuration={6000} onClose={handleClose}>
-              <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+            <Snackbar
+              open={snackbar.isOpen}
+              autoHideDuration={6000}
+              onClose={handleClose}
+            >
+              <Alert
+                onClose={handleClose}
+                severity="success"
+                sx={{ width: "100%" }}
+              >
                 Votre post a bien été publié!
               </Alert>
             </Snackbar>
